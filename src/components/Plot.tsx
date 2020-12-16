d22ce0f85ff62eb395c33848ccc97013a4f7d8b6
Arthur DELORT <arthurdelort@hotmail.fr>
2020-12-17 00:45:36 +0100

    Add input and improve style

--- src/components/Plot.tsx
+++ src/components/Plot.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { range } from 'mathjs';
+import { Input, Slider } from '@material-ui/core';
 
 export const Plot = () => {
-  const xValues = range(0, Math.PI * 2, 0.1).toArray() as number[];
+  const [f, setF] = useState<number>(1);
+
+  return (
+    <div>
+      <Chart f={f as number} />
+      <FrequencyInput f={f} setFrequency={setF} />
+    </div>
+  );
+};
+const FrequencyInput = ({
+  f,
+  setFrequency,
+}: {
+  f: number;
+  setFrequency: (f: number) => void;
+}) => {
+  return (
+    <div>
+      <Slider
+        value={typeof f === 'number' ? f : 0}
+        onChange={(_event, newValue) => {
+          const newF = typeof newValue === 'number' ? newValue : 0;
+          setFrequency(newF);
+        }}
+      />
+      <Input
+        value={f}
+        onChange={(event) => {
+          const parsedF = parseInt(event.target.value, 10);
+          const newF = isNaN(parsedF) ? 0 : parsedF;
+          setFrequency(newF);
+        }}
+        inputProps={{ min: 0, max: 100, step: 1 }}
+      />
+    </div>
+  );
+};
+
+const Chart = ({ f }: { f: number }) => {
+  const xValues = range(0, Math.PI * 2, 0.01).toArray() as number[];
 
   return (
     <div>
@@ -13,12 +53,25 @@ export const Plot = () => {
           datasets: [
             {
               label: 'Sinus',
-              data: xValues.map(Math.sin),
+              data: xValues.map((t) => Math.sin(2 * Math.PI * f * t)),
+              pointRadius: 0,
               backgroundColor: 'rgba(255, 99, 132, 0.2)',
               borderColor: 'rgba(255, 99, 132, 1)',
               borderWidth: 1,
             },
           ],
+        }}
+        options={{
+          scales: {
+            xAxes: [
+              {
+                ticks: {
+                  callback: (label: number, index: number) =>
+                    index % 20 === 0 ? label.toFixed(3) : '',
+                },
+              },
+            ],
+          },
         }}
       />
     </div>
